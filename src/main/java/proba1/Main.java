3fcb39b3a64dab8587b7ee695a704f035993a6e2
v.chilikov <v.chilikov@rambler-co.ru>
2018-07-02 23:29:17 +0300

    add rapidoid

--- src/main/java/proba1/Main.java
+++ src/main/java/proba1/Main.java
@@ -1,7 +1,10 @@
 package proba1;
 
+import org.rapidoid.setup.On;
+import org.rapidoid.u.U;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        On.get("/hello").json(() -> U.map("msg", "Hello, world!"));
     }
 }
